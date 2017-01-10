062e4daa637ffec96a0e7f06943e65c8066ba451
shy_azusa <shy.azusa@gmail.com>
2017-01-10 20:27:31 +0900

    (#15) build

--- js/option.js
+++ js/option.js
@@ -1,4 +1,5 @@
 $(function() {
+  var setMilestone;
   if (localStorage['user']) {
     $('#user').val(localStorage['user']);
   } else {
@@ -19,10 +20,58 @@ $(function() {
   } else {
     $('#labels').val('readlate, memo');
   }
-  return $('#save').on('click', function() {
+  $('.milestone').hide();
+  $('#save').on('click', function() {
     localStorage['user'] = $('#user').val();
     localStorage['repo'] = $('#repo').val();
     localStorage['token'] = $('#token').val();
-    return localStorage['labels'] = $('#labels').val();
+    localStorage['labels'] = $('#labels').val();
+    return setMilestone();
   });
+  return setMilestone = function() {
+    var query, repo, token, url, user;
+    user = localStorage['user'];
+    repo = localStorage['repo'];
+    token = localStorage['token'];
+    if (user === null || user === 'Enter the GitHub user name') {
+      user = null;
+    }
+    if (repo === null || repo === 'Enter the GitHub repositori name') {
+      repo = null;
+    }
+    if (user && repo) {
+      query = JSON.stringify({
+        'state': 'all',
+        'sort': 'due_on',
+        'direction': 'asc'
+      });
+      url = "https://api.github.com/repos/" + user + "/" + repo + "/milestones?access_token=" + token;
+      return $.ajax({
+        'async': false,
+        'url': url,
+        'type': 'get',
+        'data': query,
+        'dataType': 'json',
+        'success': function(res) {
+          var key, value;
+          $('#milestone option').remove();
+          $('#milestone').append($('<option>', {
+            value: null,
+            text: 'Choose Milestone'
+          }));
+          for (key in res) {
+            value = res[key];
+            $('#milestone').append($('<option>', {
+              value: value['number'],
+              text: value['title']
+            }));
+          }
+          return $('.milestone').show();
+        },
+        'error': function(res) {
+          return console.log(url);
+        }
+      });
+    }
+  };
 });
